b1d5f59843c2578460789cfb282e0ab7f1601208
Ahmet Altinisik <aaltinisik@altinkaya.com.tr>
2015-12-30 09:02:34 +0200

    first commits

--- install-barman-replica.sh
+++ install-barman-replica.sh
@@ -1,136 +1,167 @@
 #!/bin/bash
 # -*- encoding: utf-8 -*-
 
-# this script installs barman on database backup server 10.1.1.11
+# this script installs barman on database backup server
+BAR_CONFIG="/ect/barman.conf"
+
+echo -e "---- Decide system passwords ----"
+read -e -p "Enter Postgresql server address to backup: " PG_IP
+echo -e "\n"
+read -e -p "Enter password of postgres database user at $PG_IP: " POSTGRES_DBPASS
+echo -e "\n"
+read -e -p "Decide a password for barman system user on this machine : " BARMAN_USERPASS
+echo -e "\n"
+read -e -p "Enter password of postgres system user at $PG_IP : " PG_USERPASS
+
+
+echo -e "\n"
+echo -e "\nInstalling Barman..."
+
+
 
 #sudo apt-get update
 #sudo apt-get upgrade -y
+#sudo apt-get install pip
+#sudo pip install barman
 
-sudo apt-get install pip
-sudo pip install barman
+sudo su root -c "echo barman:$BARMAN_USERPASS | chpasswd"
 
-sudo passwd barman
+echo -e "\nYou can connect to this machine with user:barman password:$BARMAN_USERPASS"
+echo -e "\nPlease switch to PostgreSQL machine and copy ssh keys to here."
+while true; do
+    read -p "Do you want to continue (y/n)?" yn
+    case $yn in
+        [Yy]* ) break;;
+        [Nn]* ) exit 
+	break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
 
 sudo su barman
 cd ~
 ssh-keygen
-ssh-copy-id -i ~/.ssh/id_rsa.pub postgres@10.1.1.15
+
+sshpass -p '$PG_USERPASS' ssh-copy-id -i ~/.ssh/id_rsa.pub postgres@$PG_IP
+
+#ssh-copy-id -i ~/.ssh/id_rsa.pub postgres@$PG_IP
 
 touch ~/.pgpass
 chmod 600 ~/.pgpass
-echo "10.1.1.15:*:*:postgres:kt471012" >> ~/.pgpass
+echo "$PG_IP:*:*:postgres:$POSTGRES_DBPASS" >> ~/.pgpass
 
-psql -c 'SELECT version()' -U postgres -h 10.1.1.15
+echo -e "---- Try to connect PostgreSQL DB ----"
+
+psql -c 'SELECT version()' -U postgres -h $PG_IP
 
 sudo rm $BAR_CONFIG
 sudo touch $BAR_CONFIG
-sudo chmod 644 $BAR_CONFIG
+sudo chmod 777 $BAR_CONFIG
 
 echo '#!/bin/sh' >> ~/$BAR_CONFIG
-echo '
 
+echo '; Barman, Backup and Recovery Manager for PostgreSQL' >> ~/$BAR_CONFIG
+echo '; http://www.pgbarman.org/ - http://www.2ndQuadrant.com/' >> ~/$BAR_CONFIG
+echo ';' >> ~/$BAR_CONFIG
+echo '; Main configuration file' >> ~/$BAR_CONFIG
+echo ';' >> ~/$BAR_CONFIG
+echo '[barman]' >> ~/$BAR_CONFIG
+echo '; Main directory' >> ~/$BAR_CONFIG
+echo 'barman_home = /var/lib/barman' >> ~/$BAR_CONFIG
+echo ';' >> ~/$BAR_CONFIG
+echo '; Locks directory - default: %(barman_home)s' >> ~/$BAR_CONFIG
+echo ';barman_lock_directory = /var/run/barman' >> ~/$BAR_CONFIG
+echo ';' >> ~/$BAR_CONFIG
+echo '; System user' >> ~/$BAR_CONFIG
+echo 'barman_user = barman' >> ~/$BAR_CONFIG
+echo ';' >> ~/$BAR_CONFIG
+echo '; Log location' >> ~/$BAR_CONFIG
+echo 'log_file = /var/log/barman/barman.log' >> ~/$BAR_CONFIG
+echo ';' >> ~/$BAR_CONFIG
+echo '; Default compression level: possible values are None (default), bzip2, gzip or custom' >> ~/$BAR_CONFIG
+echo 'compression = gzip' >> ~/$BAR_CONFIG
+echo ';' >> ~/$BAR_CONFIG
+echo '; Incremental backup support: possible values are None (default), link or copy' >> ~/$BAR_CONFIG
+echo 'reuse_backup = link' >> ~/$BAR_CONFIG
+echo ';' >> ~/$BAR_CONFIG
+echo '; Pre/post backup hook scripts' >> ~/$BAR_CONFIG
+echo ';pre_backup_script = env | grep ^BARMAN' >> ~/$BAR_CONFIG
+echo ';pre_backup_retry_script = env | grep ^BARMAN' >> ~/$BAR_CONFIG
+echo ';post_backup_retry_script = env | grep ^BARMAN' >> ~/$BAR_CONFIG
+echo ';post_backup_script = env | grep ^BARMAN' >> ~/$BAR_CONFIG
+echo ';' >> ~/$BAR_CONFIG
+echo '; Pre/post archive hook scripts' >> ~/$BAR_CONFIG
+echo ';pre_archive_script = env | grep ^BARMAN' >> ~/$BAR_CONFIG
+echo ';pre_archive_retry_script = env | grep ^BARMAN' >> ~/$BAR_CONFIG
+echo ';post_archive_retry_script = env | grep ^BARMAN' >> ~/$BAR_CONFIG
+echo 'post_archive_script = env | grep ^BARMAN' >> ~/$BAR_CONFIG
+echo ';' >> ~/$BAR_CONFIG
+echo '; Directory of configuration files. Place your sections in separate files with .conf extension' >> ~/$BAR_CONFIG
+echo '; For example place the 'main' server section in /etc/barman.d/main.conf' >> ~/$BAR_CONFIG
+echo ';configuration_files_directory = /etc/barman.d' >> ~/$BAR_CONFIG
+echo ';' >> ~/$BAR_CONFIG
+echo '; Minimum number of required backups (redundancy) - default 0' >> ~/$BAR_CONFIG
+echo 'minimum_redundancy = 1' >> ~/$BAR_CONFIG
+echo ';' >> ~/$BAR_CONFIG
+echo '; Global retention policy (REDUNDANCY or RECOVERY WINDOW) - default empty' >> ~/$BAR_CONFIG
+echo ';retention_policy =' >> ~/$BAR_CONFIG
+echo ';' >> ~/$BAR_CONFIG
+echo '; Global bandwidth limit in KBPS - default 0 (meaning no limit)' >> ~/$BAR_CONFIG
+echo 'bandwidth_limit = 1000' >> ~/$BAR_CONFIG
+echo ';' >> ~/$BAR_CONFIG
+echo '; Immediate checkpoint for backup command - default false' >> ~/$BAR_CONFIG
+echo ';immediate_checkpoint = false' >> ~/$BAR_CONFIG
+echo ';' >> ~/$BAR_CONFIG
+echo '; Enable network compression for data transfers - default false' >> ~/$BAR_CONFIG
+echo 'network_compression = true' >> ~/$BAR_CONFIG
+echo ';' >> ~/$BAR_CONFIG
+echo '; Identify the standard behavior for backup operations: possible values are' >> ~/$BAR_CONFIG
+echo '; exclusive_backup (default), concurrent_backup' >> ~/$BAR_CONFIG
+echo ';backup_options = exclusive_backup' >> ~/$BAR_CONFIG
+echo ';' >> ~/$BAR_CONFIG
+echo '; Number of retries of data copy during base backup after an error - default 0' >> ~/$BAR_CONFIG
+echo ';basebackup_retry_times = 0' >> ~/$BAR_CONFIG
+echo ';' >> ~/$BAR_CONFIG
+echo '; Number of seconds of wait after a failed copy, before retrying - default 30' >> ~/$BAR_CONFIG
+echo ';basebackup_retry_sleep = 30' >> ~/$BAR_CONFIG
+echo ';' >> ~/$BAR_CONFIG
+echo '; Time frame that must contain the latest backup date.' >> ~/$BAR_CONFIG
+echo '; If the latest backup is older than the time frame, barman check' >> ~/$BAR_CONFIG
+echo '; command will report an error to the user.' >> ~/$BAR_CONFIG
+echo '; If empty, the latest backup is always considered valid.' >> ~/$BAR_CONFIG
+echo '; Syntax for this option is: "i (DAYS | WEEKS | MONTHS)" where i is an' >> ~/$BAR_CONFIG
+echo '; integer > 0 which identifies the number of days | weeks | months of' >> ~/$BAR_CONFIG
+echo '; validity of the latest backup for this check. Also known as 'smelly backup'.' >> ~/$BAR_CONFIG
+echo ';last_backup_maximum_age =' >> ~/$BAR_CONFIG
+echo ';' >> ~/$BAR_CONFIG
+echo ';; ; main PostgreSQL Server configuration' >> ~/$BAR_CONFIG
+echo '[main]' >> ~/$BAR_CONFIG
+echo ';; ; Human readable description' >> ~/$BAR_CONFIG
+echo 'description =  "Main PostgreSQL Database"' >> ~/$BAR_CONFIG
+echo ';;' >> ~/$BAR_CONFIG
+echo ';; ; SSH options' >> ~/$BAR_CONFIG
+echo 'ssh_command = ssh postgres@$PG_IP' >> ~/$BAR_CONFIG
+echo ';;' >> ~/$BAR_CONFIG
+echo ';; ; PostgreSQL connection string' >> ~/$BAR_CONFIG
+echo 'conninfo = host=$PG_IP user=postgres' >> ~/$BAR_CONFIG
+echo ';;' >> ~/$BAR_CONFIG
+echo ';; ; PostgreSQL streaming connection string' >> ~/$BAR_CONFIG
+echo ';;streaming_conninfo = host=pg user=postgres' >> ~/$BAR_CONFIG
+echo ';;' >> ~/$BAR_CONFIG
+echo ';; ; Minimum number of required backups (redundancy)' >> ~/$BAR_CONFIG
+echo 'minimum_redundancy = 1' >> ~/$BAR_CONFIG
+echo ';;' >> ~/$BAR_CONFIG
+echo ';; ; Examples of retention policies' >> ~/$BAR_CONFIG
+echo ';;' >> ~/$BAR_CONFIG
+echo ';; ; Retention policy (disabled)' >> ~/$BAR_CONFIG
+echo ';; ; retention_policy =' >> ~/$BAR_CONFIG
+echo ';; ; Retention policy (based on redundancy)' >> ~/$BAR_CONFIG
+echo ';; ; retention_policy = REDUNDANCY 2' >> ~/$BAR_CONFIG
+echo ';; ; Retention policy (based on recovery window)' >> ~/$BAR_CONFIG
+echo 'retention_policy = RECOVERY WINDOW OF 4 WEEKS' >> ~/$BAR_CONFIG
+echo 'retention_policy_mode = auto' >> ~/$BAR_CONFIG
+echo 'wal_retention_policy = main' >> ~/$BAR_CONFIG
 
-
-
-echo '; Barman, Backup and Recovery Manager for PostgreSQL
-echo '; http://www.pgbarman.org/ - http://www.2ndQuadrant.com/
-echo ';
-echo '; Main configuration file
-echo '
-echo '[barman]
-echo '; Main directory
-echo 'barman_home = /var/lib/barman
-
-echo '; Locks directory - default: %(barman_home)s
-echo ';barman_lock_directory = /var/run/barman
-echo '
-echo '; System user
-echo 'barman_user = barman
-
-echo '; Log location
-echo 'log_file = /var/log/barman/barman.log
-
-echo '; Default compression level: possible values are None (default), bzip2, gzip or$
-echo 'compression = gzip
-
-echo '; Incremental backup support: possible values are None (default), link or copy
-echo 'reuse_backup = link
-
-echo '; Pre/post backup hook scripts
-echo ';pre_backup_script = env | grep ^BARMAN
-echo ';pre_backup_retry_script = env | grep ^BARMAN
-echo ';post_backup_retry_script = env | grep ^BARMAN
-echo ';post_backup_script = env | grep ^BARMAN
-
-echo '; Pre/post archive hook scripts
-echo ';pre_archive_script = env | grep ^BARMAN
-echo ';pre_archive_retry_script = env | grep ^BARMAN
-echo ';post_archive_retry_script = env | grep ^BARMAN
-echo 'post_archive_script = env | grep ^BARMAN
-
-echo '; Directory of configuration files. Place your sections in separate files with $
-echo '; For example place the 'main' server section in /etc/barman.d/main.conf
-echo ';configuration_files_directory = /etc/barman.d
-echo '
-echo '; Minimum number of required backups (redundancy) - default 0
-echo 'minimum_redundancy = 1
-echo '
-echo '; Global retention policy (REDUNDANCY or RECOVERY WINDOW) - default empty
-echo ';retention_policy =
-
-echo '; Global bandwidth limit in KBPS - default 0 (meaning no limit)
-echo 'bandwidth_limit = 4000
-
-echo '; Immediate checkpoint for backup command - default false
-;immediate_checkpoint = false
-
-; Enable network compression for data transfers - default false
-network_compression = true
-
-; Identify the standard behavior for backup operations: possible values are
-; exclusive_backup (default), concurrent_backup
-;backup_options = exclusive_backup
-
-; Number of retries of data copy during base backup after an error - default 0
-;basebackup_retry_times = 0
-
-; Number of seconds of wait after a failed copy, before retrying - default 30
-;basebackup_retry_sleep = 30
-
-; Time frame that must contain the latest backup date.
-; If the latest backup is older than the time frame, barman check
-; command will report an error to the user.
-; If empty, the latest backup is always considered valid.
-; Syntax for this option is: "i (DAYS | WEEKS | MONTHS)" where i is an
-; integer > 0 which identifies the number of days | weeks | months of
-; validity of the latest backup for this check. Also known as 'smelly backup'.
-;last_backup_maximum_age =
-
-;; ; 'main' PostgreSQL Server configuration
-[main]
-;; ; Human readable description
-description =  "Main PostgreSQL Database"
-;;
-;; ; SSH options
-ssh_command = ssh postgres@10.1.1.15
-;;
-;; ; PostgreSQL connection string
-conninfo = host=10.1.1.15 user=postgres
-;;
-;; ; PostgreSQL streaming connection string
-;;streaming_conninfo = host=pg user=postgres
-;;
-;; ; Minimum number of required backups (redundancy)
-minimum_redundancy = 1
-;;
-;; ; Examples of retention policies
-;;
-;; ; Retention policy (disabled)
-;; ; retention_policy =
-;; ; Retention policy (based on redundancy)
-;; ; retention_policy = REDUNDANCY 2
-;; ; Retention policy (based on recovery window)
-retention_policy = RECOVERY WINDOW OF 4 WEEKS
-retention_policy_mode = auto
-wal_retention_policy = main
+sudo chmod 644 $BAR_CONFIG
 
--- install-barman-source.sh
+++ install-barman-source.sh
@@ -4,20 +4,16 @@
 # this script installs barman on source database computer 10.1.1.15 and backup is 10.1.1.11
 
 echo -e "---- Decide system passwords ----"
-read -e  -p "Enter a strong  password for postgreSQL postgres user for remote connection: " POSTGRES_USERPASS
+read -e  -p "Decide a strong  password for postgreSQL postgres database user for remote connection: " POSTGRES_DBPASS
 echo -e "\n"
+read -e -p "Decide a password for postgres system ssh user on this machine : " PG_USERPASS
+
 read -e  -p "Enter backup server name: " BACKUP_IP
 echo -e "\n"
 
-#sudo apt-get update
-#sudo apt-get upgrade -y
+sudo -u postgres psql -U postgres -d postgres -c "alter user postgres with password '$POSTGRES_DBPASS';"
 
-#sudo apt-get install pip
-sudo pip install barman
-
-sudo -u postgres psql -U postgres -d postgres -c "alter user postgres with password '$POSTGRES_USERPASS';"
-
-sudo su root -c "echo 'host    all             all             10.1.1.11/29             md5' >> /etc/postgresql/9.3/main/pg_hba.conf"
+sudo su root -c "echo 'host    all             all             $BACKUPIP/32             md5' >> /etc/postgresql/9.3/main/pg_hba.conf"
 
 sudo sed -i s/"#wal_level = minimal'"/"wal_level = archive"/g /etc/postgresql/9.3/main/postgresql.conf
 sudo sed -i s/"#archive_mode = off'"/"archive_mode = on"/g /etc/postgresql/9.3/main/postgresql.conf
@@ -28,7 +24,8 @@ sudo su postgres -c "ssh-keygen"
 sudo su postgres -c "ssh-copy-id -i ~/.ssh/id_rsa.pub barman@$BACKUP_IP"
 
 echo -e "\nPlease move to the barman backup machine and run install script give credentials below"
-echo -e "\npostgres db user pass :$POSTGRES_USERPASS"
+echo -e "\npostgres db user pass :$POSTGRES_DBPASS"
+echo -e "\npostgres ssh system user pass :$PG_USERPASS"
 echo -e "\nWhen prompted on backup machine continue this script to finalize"
 while true; do
     read -p "Is backup machine ready for ssh connection, proceed (y/n)?" yn
@@ -42,14 +39,10 @@ while true; do
 done
 
 
-
-
-echo -e "\n >>>>>>>>>> PLEASE RESTART YOUR SERVER TO FINALISE THE INSTALLATION (See below for the command you should use) <<<<<<<<<<"
-echo -e "\n---- restart the server (sudo shutdown -r now) ----"
 while true; do
-    read -p "Would you like to restart your server now (y/n)?" yn
+    read -p "Would you like to restart your postgreSQL server now (y/n)?" yn
     case $yn in
-        [Yy]* ) sudo shutdown -r now
+        [Yy]* ) sudo service postgresql restart
         break;;
         [Nn]* ) break;;
         * ) echo "Please answer yes or no.";;
