08c2bfef23b0f2c646679ba3f5a39055b6584de3
Ahmet Altinisik <aaltinisik@altinkaya.com.tr>
2015-12-31 08:50:40 +0200

    corrected error on barman.conf

--- install-barman-replica.sh
+++ install-barman-replica.sh
@@ -50,107 +50,107 @@ sudo rm $BAR_CONFIG
 sudo touch $BAR_CONFIG
 
 sudo su root -c "echo '#!/bin/sh' >> $BAR_CONFIG"
-echo '; Barman, Backup and Recovery Manager for PostgreSQL' >> $BAR_CONFIG
-echo '; http://www.pgbarman.org/ - http://www.2ndQuadrant.com/' >> $BAR_CONFIG
-echo ';' >> $BAR_CONFIG
-echo '; Main configuration file' >> $BAR_CONFIG
-echo ';' >> $BAR_CONFIG
-echo '[barman]' >> $BAR_CONFIG
-echo '; Main directory' >> $BAR_CONFIG
-echo 'barman_home = /var/lib/barman' >> $BAR_CONFIG
-echo ';' >> $BAR_CONFIG
-echo '; Locks directory - default: %(barman_home)s' >> $BAR_CONFIG
-echo ';barman_lock_directory = /var/run/barman' >> $BAR_CONFIG
-echo ';' >> $BAR_CONFIG
-echo '; System user' >> $BAR_CONFIG
-echo 'barman_user = barman' >> $BAR_CONFIG
-echo ';' >> $BAR_CONFIG
-echo '; Log location' >> $BAR_CONFIG
-echo 'log_file = /var/log/barman/barman.log' >> $BAR_CONFIG
-echo ';' >> $BAR_CONFIG
-echo '; Default compression level: possible values are None (default), bzip2, gzip or custom' >> $BAR_CONFIG
-echo 'compression = gzip' >> $BAR_CONFIG
-echo ';' >> $BAR_CONFIG
-echo '; Incremental backup support: possible values are None (default), link or copy' >> $BAR_CONFIG
-echo 'reuse_backup = link' >> $BAR_CONFIG
-echo ';' >> $BAR_CONFIG
-echo '; Pre/post backup hook scripts' >> $BAR_CONFIG
-echo ';pre_backup_script = env | grep ^BARMAN' >> $BAR_CONFIG
-echo ';pre_backup_retry_script = env | grep ^BARMAN' >> $BAR_CONFIG
-echo ';post_backup_retry_script = env | grep ^BARMAN' >> $BAR_CONFIG
-echo ';post_backup_script = env | grep ^BARMAN' >> $BAR_CONFIG
-echo ';' >> $BAR_CONFIG
-echo '; Pre/post archive hook scripts' >> $BAR_CONFIG
-echo ';pre_archive_script = env | grep ^BARMAN' >> $BAR_CONFIG
-echo ';pre_archive_retry_script = env | grep ^BARMAN' >> $BAR_CONFIG
-echo ';post_archive_retry_script = env | grep ^BARMAN' >> $BAR_CONFIG
-echo ';post_archive_script = env | grep ^BARMAN' >> $BAR_CONFIG
-echo ';' >> $BAR_CONFIG
-echo '; Directory of configuration files. Place your sections in separate files with .conf extension' >> $BAR_CONFIG
-echo '; For example place the main server section in /etc/barman.d/main.conf' >> $BAR_CONFIG
-echo ';configuration_files_directory = /etc/barman.d' >> $BAR_CONFIG
-echo ';' >> $BAR_CONFIG
-echo '; Minimum number of required backups (redundancy) - default 0' >> $BAR_CONFIG
-echo 'minimum_redundancy = 1' >> $BAR_CONFIG
-echo ';' >> $BAR_CONFIG
-echo '; Global retention policy (REDUNDANCY or RECOVERY WINDOW) - default empty' >> $BAR_CONFIG
-echo ';retention_policy =' >> $BAR_CONFIG
-echo ';' >> $BAR_CONFIG
-echo '; Global bandwidth limit in KBPS - default 0 (meaning no limit)' >> $BAR_CONFIG
-echo 'bandwidth_limit = 1000' >> $BAR_CONFIG
-echo ';' >> $BAR_CONFIG
-echo '; Immediate checkpoint for backup command - default false' >> $BAR_CONFIG
-echo ';immediate_checkpoint = false' >> $BAR_CONFIG
-echo ';' >> $BAR_CONFIG
-echo '; Enable network compression for data transfers - default false' >> $BAR_CONFIG
-echo 'network_compression = true' >> $BAR_CONFIG
-echo ';' >> $BAR_CONFIG
-echo '; Identify the standard behavior for backup operations: possible values are' >> $BAR_CONFIG
-echo '; exclusive_backup (default), concurrent_backup' >> $BAR_CONFIG
-echo ';backup_options = exclusive_backup' >> $BAR_CONFIG
-echo ';' >> $BAR_CONFIG
-echo '; Number of retries of data copy during base backup after an error - default 0' >> $BAR_CONFIG
-echo ';basebackup_retry_times = 0' >> $BAR_CONFIG
-echo ';' >> $BAR_CONFIG
-echo '; Number of seconds of wait after a failed copy, before retrying - default 30' >> $BAR_CONFIG
-echo ';basebackup_retry_sleep = 30' >> $BAR_CONFIG
-echo ';' >> $BAR_CONFIG
-echo '; Time frame that must contain the latest backup date.' >> $BAR_CONFIG
-echo '; If the latest backup is older than the time frame, barman check' >> $BAR_CONFIG
-echo '; command will report an error to the user.' >> $BAR_CONFIG
-echo '; If empty, the latest backup is always considered valid.' >> $BAR_CONFIG
-echo '; Syntax for this option is: "i (DAYS | WEEKS | MONTHS)" where i is an' >> $BAR_CONFIG
-echo '; integer > 0 which identifies the number of days | weeks | months of' >> $BAR_CONFIG
-echo '; validity of the latest backup for this check. Also known as smelly backup.' >> $BAR_CONFIG
-echo ';last_backup_maximum_age =' >> $BAR_CONFIG
-echo ';' >> $BAR_CONFIG
-echo ';; ; main PostgreSQL Server configuration' >> $BAR_CONFIG
-echo '[main]' >> $BAR_CONFIG
-echo ';; ; Human readable description' >> $BAR_CONFIG
-echo 'description =  "Main PostgreSQL Database"' >> $BAR_CONFIG
-echo ';;' >> $BAR_CONFIG
-echo ';; ; SSH options' >> $BAR_CONFIG
-echo 'ssh_command = ssh postgres@$PG_IP' >> $BAR_CONFIG
-echo ';;' >> $BAR_CONFIG
-echo ';; ; PostgreSQL connection string' >> $BAR_CONFIG
+sudo su root -c "echo '; Barman, Backup and Recovery Manager for PostgreSQL' >> $BAR_CONFIG"
+sudo su root -c "echo '; http://www.pgbarman.org/ - http://www.2ndQuadrant.com/' >> $BAR_CONFIG"
+sudo su root -c "echo ';' >> $BAR_CONFIG"
+sudo su root -c "echo '; Main configuration file' >> $BAR_CONFIG"
+sudo su root -c "echo ';' >> $BAR_CONFIG"
+sudo su root -c "echo '[barman]' >> $BAR_CONFIG"
+sudo su root -c "echo '; Main directory' >> $BAR_CONFIG"
+sudo su root -c "echo 'barman_home = /var/lib/barman' >> $BAR_CONFIG"
+sudo su root -c "echo ';' >> $BAR_CONFIG"
+sudo su root -c "echo '; Locks directory - default: %(barman_home)s' >> $BAR_CONFIG"
+sudo su root -c "echo ';barman_lock_directory = /var/run/barman' >> $BAR_CONFIG"
+sudo su root -c "echo ';' >> $BAR_CONFIG"
+sudo su root -c "echo '; System user' >> $BAR_CONFIG"
+sudo su root -c "echo 'barman_user = barman' >> $BAR_CONFIG"
+sudo su root -c "echo ';' >> $BAR_CONFIG"
+sudo su root -c "echo '; Log location' >> $BAR_CONFIG"
+sudo su root -c "echo 'log_file = /var/log/barman/barman.log' >> $BAR_CONFIG"
+sudo su root -c "echo ';' >> $BAR_CONFIG"
+sudo su root -c "echo '; Default compression level: possible values are None (default), bzip2, gzip or custom' >> $BAR_CONFIG"
+sudo su root -c "echo 'compression = gzip' >> $BAR_CONFIG"
+sudo su root -c "echo ';' >> $BAR_CONFIG"
+sudo su root -c "echo '; Incremental backup support: possible values are None (default), link or copy' >> $BAR_CONFIG"
+sudo su root -c "echo 'reuse_backup = link' >> $BAR_CONFIG"
+sudo su root -c "echo ';' >> $BAR_CONFIG"
+sudo su root -c "echo '; Pre/post backup hook scripts' >> $BAR_CONFIG"
+sudo su root -c "echo ';pre_backup_script = env | grep ^BARMAN' >> $BAR_CONFIG"
+sudo su root -c "echo ';pre_backup_retry_script = env | grep ^BARMAN' >> $BAR_CONFIG"
+sudo su root -c "echo ';post_backup_retry_script = env | grep ^BARMAN' >> $BAR_CONFIG"
+sudo su root -c "echo ';post_backup_script = env | grep ^BARMAN' >> $BAR_CONFIG"
+sudo su root -c "echo ';' >> $BAR_CONFIG"
+sudo su root -c "echo '; Pre/post archive hook scripts' >> $BAR_CONFIG"
+sudo su root -c "echo ';pre_archive_script = env | grep ^BARMAN' >> $BAR_CONFIG"
+sudo su root -c "echo ';pre_archive_retry_script = env | grep ^BARMAN' >> $BAR_CONFIG"
+sudo su root -c "echo ';post_archive_retry_script = env | grep ^BARMAN' >> $BAR_CONFIG"
+sudo su root -c "echo ';post_archive_script = env | grep ^BARMAN' >> $BAR_CONFIG"
+sudo su root -c "echo ';' >> $BAR_CONFIG"
+sudo su root -c "echo '; Directory of configuration files. Place your sections in separate files with .conf extension' >> $BAR_CONFIG"
+sudo su root -c "echo '; For example place the main server section in /etc/barman.d/main.conf' >> $BAR_CONFIG"
+sudo su root -c "echo ';configuration_files_directory = /etc/barman.d' >> $BAR_CONFIG"
+sudo su root -c "echo ';' >> $BAR_CONFIG"
+sudo su root -c "echo '; Minimum number of required backups (redundancy) - default 0' >> $BAR_CONFIG"
+sudo su root -c "echo 'minimum_redundancy = 1' >> $BAR_CONFIG"
+sudo su root -c "echo ';' >> $BAR_CONFIG"
+sudo su root -c "echo '; Global retention policy (REDUNDANCY or RECOVERY WINDOW) - default empty' >> $BAR_CONFIG"
+sudo su root -c "echo ';retention_policy =' >> $BAR_CONFIG"
+sudo su root -c "echo ';' >> $BAR_CONFIG"
+sudo su root -c "echo '; Global bandwidth limit in KBPS - default 0 (meaning no limit)' >> $BAR_CONFIG"
+sudo su root -c "echo 'bandwidth_limit = 1000' >> $BAR_CONFIG"
+sudo su root -c "echo ';' >> $BAR_CONFIG"
+sudo su root -c "echo '; Immediate checkpoint for backup command - default false' >> $BAR_CONFIG"
+sudo su root -c "echo ';immediate_checkpoint = false' >> $BAR_CONFIG"
+sudo su root -c "echo ';' >> $BAR_CONFIG"
+sudo su root -c "echo '; Enable network compression for data transfers - default false' >> $BAR_CONFIG"
+sudo su root -c "echo 'network_compression = true' >> $BAR_CONFIG"
+sudo su root -c "echo ';' >> $BAR_CONFIG"
+sudo su root -c "echo '; Identify the standard behavior for backup operations: possible values are' >> $BAR_CONFIG"
+sudo su root -c "echo '; exclusive_backup (default), concurrent_backup' >> $BAR_CONFIG"
+sudo su root -c "echo ';backup_options = exclusive_backup' >> $BAR_CONFIG"
+sudo su root -c "echo ';' >> $BAR_CONFIG"
+sudo su root -c "echo '; Number of retries of data copy during base backup after an error - default 0' >> $BAR_CONFIG"
+sudo su root -c "echo ';basebackup_retry_times = 0' >> $BAR_CONFIG"
+sudo su root -c "echo ';' >> $BAR_CONFIG"
+sudo su root -c "echo '; Number of seconds of wait after a failed copy, before retrying - default 30' >> $BAR_CONFIG"
+sudo su root -c "echo ';basebackup_retry_sleep = 30' >> $BAR_CONFIG"
+sudo su root -c "echo ';' >> $BAR_CONFIG"
+sudo su root -c "echo '; Time frame that must contain the latest backup date.' >> $BAR_CONFIG"
+sudo su root -c "echo '; If the latest backup is older than the time frame, barman check' >> $BAR_CONFIG"
+sudo su root -c "echo '; command will report an error to the user.' >> $BAR_CONFIG"
+sudo su root -c "echo '; If empty, the latest backup is always considered valid.' >> $BAR_CONFIG"
+sudo su root -c "echo '; Syntax for this option is: "i (DAYS | WEEKS | MONTHS)" where i is an' >> $BAR_CONFIG"
+sudo su root -c "echo '; integer > 0 which identifies the number of days | weeks | months of' >> $BAR_CONFIG"
+sudo su root -c "echo '; validity of the latest backup for this check. Also known as smelly backup.' >> $BAR_CONFIG"
+sudo su root -c "echo ';last_backup_maximum_age =' >> $BAR_CONFIG"
+sudo su root -c "echo ';' >> $BAR_CONFIG"
+sudo su root -c "echo ';; ; main PostgreSQL Server configuration' >> $BAR_CONFIG"
+sudo su root -c "echo '[main]' >> $BAR_CONFIG"
+sudo su root -c "echo ';; ; Human readable description' >> $BAR_CONFIG"
+sudo su root -c "echo 'description =  "Main PostgreSQL Database"' >> $BAR_CONFIG"
+sudo su root -c "echo ';;' >> $BAR_CONFIG"
+sudo su root -c "echo ';; ; SSH options' >> $BAR_CONFIG"
+sudo su root -c "echo 'ssh_command = ssh postgres@$PG_IP' >> $BAR_CONFIG"
+sudo su root -c "echo ';;' >> $BAR_CONFIG"
+sudo su root -c "echo ';; ; PostgreSQL connection string' >> $BAR_CONFIG"
 sudo su root -c "echo 'conninfo = host=$PG_IP user=postgres' >> $BAR_CONFIG"
-echo ';;' >> $BAR_CONFIG
-echo ';; ; PostgreSQL streaming connection string' >> $BAR_CONFIG
-echo ';;streaming_conninfo = host=pg user=postgres' >> $BAR_CONFIG
-echo ';;' >> $BAR_CONFIG
-echo ';; ; Minimum number of required backups (redundancy)' >> $BAR_CONFIG
-echo 'minimum_redundancy = 1' >> $BAR_CONFIG
-echo ';;' >> $BAR_CONFIG
-echo ';; ; Examples of retention policies' >> $BAR_CONFIG
-echo ';;' >> $BAR_CONFIG
-echo ';; ; Retention policy (disabled)' >> $BAR_CONFIG
-echo ';; ; retention_policy =' >> $BAR_CONFIG
-echo ';; ; Retention policy (based on redundancy)' >> $BAR_CONFIG
-echo ';; ; retention_policy = REDUNDANCY 2' >> $BAR_CONFIG
-echo ';; ; Retention policy (based on recovery window)' >> $BAR_CONFIG
-echo 'retention_policy = RECOVERY WINDOW OF 4 WEEKS' >> $BAR_CONFIG
-echo 'retention_policy_mode = auto' >> $BAR_CONFIG
-echo 'wal_retention_policy = main' >> $BAR_CONFIG
+sudo su root -c "echo ';;' >> $BAR_CONFIG"
+sudo su root -c "echo ';; ; PostgreSQL streaming connection string' >> $BAR_CONFIG"
+sudo su root -c "echo ';;streaming_conninfo = host=pg user=postgres' >> $BAR_CONFIG"
+sudo su root -c "echo ';;' >> $BAR_CONFIG"
+sudo su root -c "echo ';; ; Minimum number of required backups (redundancy)' >> $BAR_CONFIG"
+sudo su root -c "echo 'minimum_redundancy = 1' >> $BAR_CONFIG"
+sudo su root -c "echo ';;' >> $BAR_CONFIG"
+sudo su root -c "echo ';; ; Examples of retention policies' >> $BAR_CONFIG"
+sudo su root -c "echo ';;' >> $BAR_CONFIG"
+sudo su root -c "echo ';; ; Retention policy (disabled)' >> $BAR_CONFIG"
+sudo su root -c "echo ';; ; retention_policy =' >> $BAR_CONFIG"
+sudo su root -c "echo ';; ; Retention policy (based on redundancy)' >> $BAR_CONFIG"
+sudo su root -c "echo ';; ; retention_policy = REDUNDANCY 2' >> $BAR_CONFIG"
+sudo su root -c "echo ';; ; Retention policy (based on recovery window)' >> $BAR_CONFIG"
+sudo su root -c "echo 'retention_policy = RECOVERY WINDOW OF 4 WEEKS' >> $BAR_CONFIG"
+sudo su root -c "echo 'retention_policy_mode = auto' >> $BAR_CONFIG"
+sudo su root -c "echo 'wal_retention_policy = main' >> $BAR_CONFIG"
 
 sudo chmod 644 $BAR_CONFIG
 
